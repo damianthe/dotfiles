9b8018bb2f6cb3cedff4c67224c63140a162626a
Damian Polan <damian.polan@shopify.com>
2021-08-30 16:55:05 -0400

    clone aliases and init.vim

---
+++ setup.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/damianpolan/vim-configuration.git
+
+mv ~/dotfiles/vim-configuration/.aliases ~/.aliases
+mv -p ~/dotfiles/vim-configuration/init.vim ~/.config/nvim/init.vim
+
