12d124ed37830b162d1a7eaa216e9770427427ce
Damian Polan <damian.polan@shopify.com>
2021-08-30 17:03:59 -0400

    make parents dirs

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 git clone https://github.com/damianpolan/vim-configuration.git
 
 mv ~/dotfiles/vim-configuration/.aliases ~/.aliases
+mkdir --parents ~/.config/nvim/
 mv -p ~/dotfiles/vim-configuration/init.vim ~/.config/nvim/init.vim
 
