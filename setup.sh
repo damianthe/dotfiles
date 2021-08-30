179867a50944acb44e1c15ec1a731c69052a7eaf
Damian Polan <damian.polan@shopify.com>
2021-08-30 17:17:40 -0400

    Install plugins vim

--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
 git clone https://github.com/damianpolan/vim-configuration.git
 
+# aliases
 mv ~/dotfiles/vim-configuration/.aliases ~/.aliases
+
+# vim
 mkdir --parents ~/.config/nvim/
-mv -p ~/dotfiles/vim-configuration/init.vim ~/.config/nvim/init.vim
+mv ~/dotfiles/vim-configuration/init.vim ~/.config/nvim/init.vim
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+nvim +'PluginInstall' +qa
+
 
